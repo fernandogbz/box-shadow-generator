9379d4e5975a79d948acca05d3061bd8d7236fb2
Fernando Jimenez <fgjv06@gmail.com>
2023-02-17 19:17:37 -0300

    JS - inputs.forEach

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let element = document.getElementById("element");
 let code = document.getElementById("code");
 let inputs = document.querySelectorAll(".sliders input");
+
+inputs.forEach((input) => input.addEventListener("input", generateShadow));
+
